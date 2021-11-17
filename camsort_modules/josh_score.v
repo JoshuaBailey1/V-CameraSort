7d5a3b487f583f21033e5e053a7bf14109b9acba
Joshua Bailey <peanut_bailey902@aol.com>
2021-11-17 16:39:33 -0500

    last one

---
+++ camsort_modules/josh_score.v
@@ -0,0 +1,35 @@
+module camsort_modules
+
+fn calculate_joshs_score(data Camera_data, budget int) int
+{
+	mut score := 1000
+	if data.launch_price > budget
+	{
+		for i:= data.launch_price; i>budget; i-=100
+		{
+			score -= 10
+		}
+	}
+	if data.landscape_score > 1
+	{
+		for i:= data.landscape_score; i>1; i--
+		{
+			score -= 2
+		}
+	}
+	if data.megapixels < 24
+	{
+		for i:= data.megapixels; i<20; i+=3
+		{
+			score -= 50
+		}
+	}
+	else if data.megapixels > 24
+	{
+		for i:= data.megapixels; i>24; i-=3
+		{
+			score += 10
+		}	
+	}
+	return score
+}
--- camsort_modules/rank_calculations.v
+++ camsort_modules/rank_calculations.v
@@ -22,37 +22,3 @@ pub fn calculate_overall_rank(mut data_list[] Camera_data)
 		data_list[i-1].overall_score = i
 	}
 }
-
-fn calculate_joshs_score(data Camera_data, budget int) int
-{
-	mut score := 1000
-	if data.launch_price > budget
-	{
-		for i:= data.launch_price; i>budget; i-=100
-		{
-			score -= 10
-		}
-	}
-	if data.landscape_score > 1
-	{
-		for i:= data.landscape_score; i>1; i--
-		{
-			score -= 2
-		}
-	}
-	if data.megapixels < 24
-	{
-		for i:= data.megapixels; i<20; i+=3
-		{
-			score -= 50
-		}
-	}
-	else if data.megapixels > 24
-	{
-		for i:= data.megapixels; i>24; i-=3
-		{
-			score += 10
-		}	
-	}
-	return score
-}
